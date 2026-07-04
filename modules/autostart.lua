099899d7efb41ace091e179e7ed85fd0a62feb63
fettabit <143643888+fettabit@users.noreply.github.com>
2026-07-03 23:26:57 -0400

    chore: add awww-daemon to autostart

--- modules/autostart.lua
+++ modules/autostart.lua
@@ -2,8 +2,8 @@
 ---- AUTOSTART ----
 -------------------
 
-hl.on("hyprland.start", function ()
-    hl.exec_cmd("waybar")
-    hl.exec_cmd("swaync")
+hl.on("hyprland.start", function()
+	hl.exec_cmd("waybar")
+	hl.exec_cmd("swaync")
+	hl.exec_cmd("awww-daemon")
 end)
-
